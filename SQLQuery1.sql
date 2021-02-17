a0cadd91fd00c60d219fe49e076145b666c6d28c
Javid <68476365+avidim@users.noreply.github.com>
2021-02-17 16:28:51 +0400

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+﻿create table [dbo].[data] (
+    [ID]                        INT NOT NULL IDENTITY,
+    [Question]					NVARCHAR(MAX)     NOT NULL,
+    [Answer_1]                  NVARCHAR(MAX)	  NOT NULL,
+    [Answer_2]                  NVARCHAR(MAX)     NOT NULL,
+    [Answer_3]                  NVARCHAR(MAX)     NOT NULL,
+    [Answer_4]                  NVARCHAR(MAX)     NOT NULL,
+	[Answer_Right]				NVARCHAR(MAX)     NOT NULL,
+	PRIMARY KEY CLUSTERED ([ID] ASC)
+);
